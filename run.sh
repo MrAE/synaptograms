43e7a88a1c0a3c3d1021f1893ae977f32b3ba966
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2017-11-29 14:38:27 -0500

    add get arguments from config file

--- run.sh
+++ run.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
-C='collman'
-E='collman14v2'
-F='collman_collman14v2'
-B='108 108 5'
-L='home/xyzLocations.csv'
-O='home/testing.csv'
-o='home/synTest'
-con='home/config.ini'
+#C='collman'
+#E='collman14v2'
+#F='collman_collman14v2'
+#B='108 108 5'
+#L='home/xyzLocations.csv'
+#O='home/testing.csv'
+#o='home/synTest'
+#con='home/config.ini'
+#
+#getCube=true
+#genSynaptograms=true
 
-getCube=true
-genSynaptograms=true
-
+. ./home/config.conf
 
 if $getCube
 then
   echo "Downloading cubes"
-  python3 getCubes.py -C $C -E $E -F $F -B $B -L $L -O $O --con $con
+  python3 getCubes.py -C $COLL -E $EXP -F $FM -B $BUFF -L /home/$LOC -O /home/$OUT --con /home/$CON
   echo "Done"
 fi
 
 if $genSynaptograms
 then
   echo "Generating synaptograms"
-  Rscript Synaptograms.R -f $O.h5 -o $o
+  Rscript Synaptograms.R -f /home/$SYIN -o /home/$SYOUT
   echo "Done"
 fi
 
